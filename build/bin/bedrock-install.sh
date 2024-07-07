7f822d537db3d39d30a2c19130b4c633cfeb11ec
qusai <qusai.aqrabawe@softylus.com>
2024-07-07 12:14:45 +0300

    update docker compose

--- build/bin/bedrock-install.sh
+++ build/bin/bedrock-install.sh
@@ -8,7 +8,7 @@ cd /srv/bedrock
 su bedrockuser -c 'composer install --no-dev --optimize-autoloader'
 
 # Initialize WordPress if it's not already installed
-su bedrockuser -c 'wp core is-installed || wp core install --url=$WP_HOME --title="Bedrock Site" --admin_user=admin --admin_password=admin_password --admin_email=admin@example.com'
+su bedrockuser -c 'wp core is-installed || wp core install --url='"$WP_HOME"' --title="Bedrock Site" --admin_user=admin --admin_password=admin_password --admin_email=admin@example.com'
 
 # Install and activate the login command as bedrockuser
 su bedrockuser -c 'wp package install aaemnnosttv/wp-cli-login-command || echo "wp-cli-login-command is already installed"'
